8da195e4e3950281ccd25cc7083a018f3bc91e9a
bloyscode <bloyskiep@gmail.com>
2024-11-07 17:30:46 +0800

    last

--- database/seeders/AssignRoleSeeder.php
+++ database/seeders/AssignRoleSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Spatie\Permission\Models\Role;
 class AssignRoleSeeder extends Seeder
 {
     /**
@@ -13,5 +13,8 @@ class AssignRoleSeeder extends Seeder
     public function run(): void
     {
         //
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'judge']);
+        Role::create(['name' => 'staff']);
     }
 }
